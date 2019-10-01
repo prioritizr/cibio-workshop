895b848abf9b18c4a55ee55487c875e73ee892e5
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2019-10-01 11:47:01 +0100

    make only site

--- _build.sh
+++ _build.sh
@@ -3,4 +3,6 @@
 set -ev
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+# export MANUALVERSION=TEACHER
+# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book', config_file='_bookdown2.yml')"
