2c7e1f605f8f30bdae75109e0a1956b9ddf03f30
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2019-10-01 14:58:16 +0100

    fix website

--- _build.sh
+++ _build.sh
@@ -3,6 +3,7 @@
 set -ev
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-# export MANUALVERSION=TEACHER
-# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book', config_file='_bookdown2.yml')"
+cp -R images _book/images
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+export MANUALVERSION=TEACHER
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book', config_file='_bookdown2.yml')"
